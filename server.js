0be2623d947710841e4aa344d3182a72778748f0
morrissey-trebley <morrissey.trebley@gmail.com>
2024-08-14 15:36:22 +1000

    Added mapped volume tracking (nodemon)

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const app = express();
 app.get('/', (req, res) => {
     // Run the system 'fortune' command and respond with the message
     child_process.exec('fortune', (error, message) => {
-        let full_message = '';
+        let full_message = 'Your fortune is: "';
         if(error === null) {
             full_message += message;
         } else {
             full_message += error;
         }
-        full_message += '<br>' + Date()
+        full_message += '"<br>' + Date()
         res.send(full_message)
     })
 });
